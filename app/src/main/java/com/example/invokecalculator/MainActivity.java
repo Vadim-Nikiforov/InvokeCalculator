e87d7326fc11c0ed7b3c34bf2be85355c4433c97
Vadim-Nikiforov <rockof222@mail.ru>
2021-06-07 02:24:51 +0700

    init commit

--- app/src/main/java/com/example/invokecalculator/MainActivity.java
+++ app/src/main/java/com/example/invokecalculator/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,33 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button run = findViewById(R.id.button);
-        run.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("example://intent");
-                Intent runIntent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(runIntent);
-
-                ActivityInfo activityInfo = runIntent.resolveActivityInfo(getPackageManager(), runIntent.getFlags());
-                if (activityInfo != null) {
-                    startActivity(runIntent);
-                }
-            }
-        });
+        run.setOnClickListener(v -> start());
 
         Button run2 = findViewById(R.id.button2);
-        run2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("example://intent");
-                Intent runIntent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(runIntent);
+        run2.setOnClickListener(v -> start());
+    }
 
-                ActivityInfo activityInfo = runIntent.resolveActivityInfo(getPackageManager(), runIntent.getFlags());
-                if (activityInfo != null) {
-                    startActivity(runIntent);
-                }
-            }
-        });
+    private void start (){
+        Uri uri = Uri.parse("example://intent");
+        Intent runIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(runIntent);
+
+        ActivityInfo activityInfo = runIntent.resolveActivityInfo(getPackageManager(), runIntent.getFlags());
+        if (activityInfo != null) {
+            startActivity(runIntent);
+        }
     }
 }
